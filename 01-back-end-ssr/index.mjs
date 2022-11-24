5cf1777f0b03ec49383074854c79839abcf7747f
reglabel <regla.belcl@gmail.com>
2022-11-24 14:16:40 -0300

    more things

--- 01-back-end-ssr/index.mjs
+++ 01-back-end-ssr/index.mjs
@@ -1,15 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 
 app.set('view engine', 'ejs');
 
 app.get('/users', (req, res) => {
     const users = [
-        {id: 1, name: 'Fulano da Silva'},
+        {id: 1, name: 'Bob da Silva'},
         {id: 2, name: 'Sicrano Costa'},
         {id: 3, name: 'Beltrano Orlando'}
     ];
+
+    if (req.header('Accept') === 'application/json'){
+        return res.json({data: users});
+    }
 
     return res.render('users/list', { users })
 });
--- 02-front-end-spa/src/App.tsx
+++ 02-front-end-spa/src/App.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const headers = new Headers()
+
+    headers.append('Accept', 'application/json')
+
+    const response = fetch('http://localhost:3333/users', {
+      headers 
+    })
+    .then(response => response.json())
+    .then(data => {
+      setUsers(data.data);
+    })
+  }, [])
+
   return (
     <>
       <ul>
-        <li>Diego Fernandes</li>
-        <li>Mayk Brito</li>
-        <li>Renan Biazini</li>
+        {users.map((user: any) => {
+          return <li key={user.id}>{user.name}</li>
+        })}
       </ul>
     </>
   )
